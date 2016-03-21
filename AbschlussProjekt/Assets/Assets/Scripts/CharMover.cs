5fc2fb9566c5b6deda2e460a0eb83beb147528de
Marco Heuser <marcoh96@web.de>
2016-03-21 16:27:19 +0100

    Character Controller v0.1 WORKING
    by Marco

---
+++ AbschlussProjekt/Assets/Assets/Scripts/CharMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMover : MonoBehaviour
+{
+    public float MovementSpeed = 5.0f;
+    private CharacterController charContr;
+
+    void Awake()
+    {
+        charContr = GetComponent<CharacterController>();
+    }
+
+	void Update()
+    {
+        Movement();
+	}
+
+    private void Movement()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        move = move.normalized;
+        move *= Time.deltaTime * MovementSpeed;
+        Debug.Log(move);
+
+        charContr.Move(move);
+    }
+}
