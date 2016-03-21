c82310090093d91fc8b744e82b9d2c20b6d0217b
Marco Heuser <marcoh96@web.de>
2016-03-21 16:56:21 +0100

    Character Controller v0.5 Working
    Basisversionen von Laufen, Springen, und Umschauen funktionieren
    by Marco

--- AbschlussProjekt/Assets/Assets/Scripts/CharMover.cs
+++ AbschlussProjekt/Assets/Assets/Scripts/CharMover.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class CharMover : MonoBehaviour
 {
     public float MovementSpeed = 5.0f;
+    public float RotationSpeed = 200.0f;
+    public float JumpStrength = 5.0f;
+    public Transform Head;
+
     private CharacterController charContr;
+    private float m_ySpeed = 0.0f;
 
     void Awake()
     {
@@ -14,6 +19,7 @@ public class CharMover : MonoBehaviour
 	void Update()
     {
         Movement();
+        Rotation();
 	}
 
     private void Movement()
@@ -21,8 +27,40 @@ public class CharMover : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         move = move.normalized;
         move *= Time.deltaTime * MovementSpeed;
-        Debug.Log(move);
+        move = transform.TransformDirection(move);
+
+
+        if (charContr.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                m_ySpeed = JumpStrength;
+            }
+            else
+            {
+                m_ySpeed = -0.1f;
+            }
+
+        }
+        else
+        {
+            m_ySpeed += Physics.gravity.y * Time.deltaTime * 0.1f;
+        }
+        move.y = m_ySpeed;
 
         charContr.Move(move);
+    }
+
+    private void Rotation()
+    {
+        float input = Input.GetAxis("Mouse X");
+        input *= RotationSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up, input);
+
+        input = Input.GetAxis("Mouse Y");
+        input *= RotationSpeed * Time.deltaTime;
+
+        Head.Rotate(Vector3.back, input);
     }
 }
